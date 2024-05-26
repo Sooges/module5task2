4f94e5268184965e89482c42568857e8bcec2273
Sooges <160008130+Sooges@users.noreply.github.com>
2024-05-26 16:29:37 +0300

    Update main.tf

--- task/main.tf
+++ task/main.tf
@@ -1,1 +1,15 @@
+module "aws_vpc_with_ec2" {
+  source         = "./aws_vpc_with_ec2"
+  azs            = ["eu-naorth-1"]
+  instance_type  = "t3.micro"                        
+  s3_bucket_name = "s3_bucket_name"          
+}
+
+output "vpc_id" {
+  value = module.aws_vpc_with_ec2.vpc_id
+}
+
+output "instance_id" {
+  value = module.aws_vpc_with_ec2.instance_id
+}
 
